f309c0cd2facbdf8016c2151e4bc15c5bfcf7c1b
KovZhu <kovzhu@qq.com>
2025-07-30 12:23:58 +1000

    SS-10 connect to MongoDB

--- backend/src/main/java/com/securesight/backend/BackendApplication.java
+++ backend/src/main/java/com/securesight/backend/BackendApplication.java
@@ -2,12 +2,14 @@ package com.securesight.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().load();
+        System.setProperty("MONGODB_PASSWORD", dotenv.get("MONGODB_PASSWORD"));
+        SpringApplication.run(BackendApplication.class, args);
+    }
 }
